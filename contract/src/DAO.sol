fd02cf1814ff5d514afd4ecd9f71b94cd024fbc3
rere <regis-graptin@sfr.fr>
2024-11-09 00:29:55 +0700

    feat: simple dao contract

--- contract/src/DAO.sol
+++ contract/src/DAO.sol
@@ -1,14 +1,67 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract DAO is ERC20 {
-    uint256 public number;
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract DAO is ERC20, ERC20Permit {
+
+    struct BTCProposal {
+        string btcAddress;
+        uint256 amount;
     }
 
-    function increment() public {
-        number++;
+    uint256 proposalIds;
+
+    event CreateBTCProposalEvent(uint256 proposalId);
+    event AcceptedBTCProposalEvent(uint256 proposalId, string btcAddress, uint256 amount);
+
+    mapping(uint256 => BTCProposal) proposals;
+    
+    // Add the moment, we collect only yes vote. The no are just not collected
+    // We need to take this modification into account on prod
+    mapping(uint256 => uint256) votingPower; 
+
+
+    constructor() ERC20("VotingToken", "VTK") ERC20Permit("VotingToken") {
+        _mint(msg.sender, 1000);
     }
+
+    function sendBTCProposal(string memory btcAddress, uint256 amount) public {
+        BTCProposal memory proposal = BTCProposal({
+            btcAddress: btcAddress,
+            amount: amount
+        });
+
+        proposals[proposalIds] = proposal;
+
+        emit CreateBTCProposalEvent(proposalIds);
+
+        proposalIds++;
+    }
+
+    function voteOnProposal(uint256 proposalId) public {
+        require(this.balanceOf(msg.sender) > 0, "No token for voting");
+        require(proposalId < proposalIds, "invalid proposal id");
+        // For prod version, check not already voted proposal and other 
+        // edge cases missing for this implementation
+        
+        
+        // Add voting power
+        votingPower[proposalId] += this.balanceOf(msg.sender);
+        
+
+        // In case we have reached our threshold
+        if (votingPower[proposalId] > (this.totalSupply() / 2 )) {
+            emit AcceptedBTCProposalEvent(
+                proposalId, 
+                proposals[proposalId].btcAddress, 
+                proposals[proposalId].amount
+            );
+        }
+
+    }
+
 }
